c3a86526c5092ab082ea91dcd14a3dc290225d0b
matador284 <marcosfrancisco_5@hotmail.com>
2026-04-13 14:25:45 -0300

    fix: Remove unused csv-parse import

--- painel/server.js
+++ painel/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import { parse } from "csv-parse/sync";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
